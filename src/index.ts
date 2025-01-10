e85a038285dbfb3617cd42042565b92e0d698d5e
LAPTOP-B29GO558\adamf <adamfinkler6@gmail.com>
2025-01-10 19:14:04 +0200

    added a git ignore file and installed axios and cors

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express from "express";
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors());
 const PORT = 3000;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send();
-  });
+  res.send("successfully sent a request to the backend! ");
+});
 
 app.listen(PORT, () => {
   console.log(`app is listening on port ${PORT}`);
